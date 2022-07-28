280949e63604169d382dd3a5d408e2fc8c06843f
linth <zhua0404@gmail.com>
2022-07-28 19:38:23 +0800

    add a script for install procedure.

---
+++ first/sh.sh
@@ -0,0 +1,15 @@
+# Ref: 
+# - https://www.youtube.com/watch?v=Rlj0xVAEoxk
+# - https://mrjk.hashnode.dev/2-nodejs
+
+# initial your project.
+npm init
+
+# download your packages or modules.
+npm install
+
+# download specific modules by yourself.
+# npm install express --save
+
+# EJS install
+npm install ejs --self
